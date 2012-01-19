850232554d5af060cd8d4d5d43eca273f8e34a19
Adam Jensen <acj@linuxguy.org>
2012-01-18 22:42:31 -0500

    Add video format parameter to video functions.
    - Refactor demo driver to use the correct formats
    - Add IR video function (in progress)

--- demo/driver.go
+++ demo/driver.go
@@ -34,7 +34,6 @@ import (
     "gonect"
     "fmt"
     "time"
-	"os"
 	"opencv"
 	"unsafe"
 )
@@ -45,89 +44,103 @@ func Run() {
     //TestTilting(0)
     //TestLed(0)
 	TestVideo(0)
-	//TestDepth(0)
+	TestIR(0)
+	TestDepth(0)
 	gonect.Stop()
 	gonect.Shutdown()
 }
 
 func TestVideo(device_index int) {
+	fmt.Println("Testing RGB video. Press ESC to stop.")
 	win := opencv.NewWindow("image")
 	for {
-		data, _ := gonect.GetVideo(0)
-		//out := C.freenect_sync_get_video(&data, &timestamp, C.int(device_index), C.FREENECT_VIDEO_RGB)
+		data, _ := gonect.GetVideo(0, gonect.FREENECT_VIDEO_RGB)
 		cedge := opencv.CreateImage(640, 480, opencv.IPL_DEPTH_8U, 3)
-		defer cedge.Release()
 		C.cvSetData(unsafe.Pointer(cedge), data, C.int(1*3*640))
 		win.ShowImage(cedge)
 		if opencv.WaitKey(10) == 27 {
-			os.Exit(0)
+			return
+        }
+	}
+}
+
+func TestIR(device_index int) {
+	fmt.Println("Testing depth. Press ESC to stop.")
+	win := opencv.NewWindow("image")
+	for {
+		data, _ := gonect.GetDepth(0, gonect.FREENECT_VIDEO_IR_8BIT)
+		cedge := opencv.CreateImage(640, 488, opencv.IPL_DEPTH_8U, 1)
+		C.cvSetData(unsafe.Pointer(cedge), data, C.int(1*1*640))
+		win.ShowImage(cedge)
+		if opencv.WaitKey(10) == 27 {
+			return
         }
 	}
 }
 
 func TestDepth(device_index int) {
+	fmt.Println("Testing depth. Press ESC to stop.")
 	win := opencv.NewWindow("image")
 	for {
-		data, _ := gonect.GetDepth(0)
-		//out := C.freenect_sync_get_depth(&data, &timestamp, C.int(device_index), C.FREENECT_DEPTH_11BIT)
-		cedge := opencv.CreateImage(640, 480, opencv.IPL_DEPTH_8U, 3)
+		data, _ := gonect.GetDepth(0, gonect.FREENECT_DEPTH_11BIT)
+		cedge := opencv.CreateImage(640, 480, opencv.IPL_DEPTH_8U, 1)
 		defer cedge.Release()
-		C.cvSetData(unsafe.Pointer(cedge), data, C.int(1*3*640))
+		C.cvSetData(unsafe.Pointer(cedge), data, C.int(1*1*640))
 		win.ShowImage(cedge)
 		if opencv.WaitKey(10) == 27 {
-			os.Exit(0)
+			return
         }
 	}
 }
 
-func TestTilting() {
+func TestTilting(device_index int) {
     fmt.Println("Tilting down")
     gonect.SetTiltDegs(-30, 0)
 	ts := gonect.TiltStatusCode(0)
 	for i := 0; i < 3; i++ {
 		time.Sleep(1000000000)
-		ts = gonect.GetTiltStatus(gonect.GetTiltState(0), 0)
+		ts = gonect.GetTiltStatus(gonect.GetTiltState(0), device_index)
 		fmt.Println("\tTilt status: ", ts, "(", gonect.GetTiltDegs(gonect.GetTiltState(0)), " degrees)")
 	}
     fmt.Println("Tilting up")
     gonect.SetTiltDegs(30, 0)
 	for i := 0; i < 3; i++ {
 		time.Sleep(1000000000)
-		ts = gonect.GetTiltStatus(gonect.GetTiltState(0), 0)
+		ts = gonect.GetTiltStatus(gonect.GetTiltState(0), device_index)
 		fmt.Println("\tTilt status: ", ts, "(", gonect.GetTiltDegs(gonect.GetTiltState(0)), " degrees)")
 	}
     fmt.Println("Tilting level")
     gonect.SetTiltDegs(0, 0)
 	for i := 0; i < 3; i++ {
 		time.Sleep(1000000000)
-		ts = gonect.GetTiltStatus(gonect.GetTiltState(0), 0)
+		ts = gonect.GetTiltStatus(gonect.GetTiltState(0), device_index)
 		fmt.Println("\tTilt status: ", ts, "(", gonect.GetTiltDegs(gonect.GetTiltState(0)), " degrees)")
 	}
 }
 
-func TestLed() {
+func TestLed(device_index int) {
 	fmt.Println("Changing LED status")
 	fmt.Println("\tOFF")
-    gonect.SetLed(gonect.LED_OFF, 0)
+    gonect.SetLed(gonect.LED_OFF, device_index)
     time.Sleep(1000000000)
 	fmt.Println("\tGREEN")
-    gonect.SetLed(gonect.LED_GREEN, 0)
+    gonect.SetLed(gonect.LED_GREEN, device_index)
     time.Sleep(1000000000)
 	fmt.Println("\tRED")
-    gonect.SetLed(gonect.LED_RED, 0)
+    gonect.SetLed(gonect.LED_RED, device_index)
     time.Sleep(1000000000)
 	fmt.Println("\tYELLOW")
-    gonect.SetLed(gonect.LED_YELLOW, 0)
+    gonect.SetLed(gonect.LED_YELLOW, device_index)
     time.Sleep(1000000000)
 	fmt.Println("\tBLINK YELLOW")
-    gonect.SetLed(gonect.LED_BLINK_YELLOW, 0)
+    gonect.SetLed(gonect.LED_BLINK_YELLOW, device_index)
     time.Sleep(3000000000)
 	fmt.Println("\tBLINK_GREEN")
-    gonect.SetLed(gonect.LED_BLINK_GREEN, 0)
+    gonect.SetLed(gonect.LED_BLINK_GREEN, device_index)
     time.Sleep(3000000000)
 	fmt.Println("\tBLINK RED/YELLOW")
-    gonect.SetLed(gonect.LED_BLINK_RED_YELLOW, 0)
+    gonect.SetLed(gonect.LED_BLINK_RED_YELLOW, device_index)
     time.Sleep(3000000000)
 	fmt.Println("\tOFF")
-    gonect.SetLed(gonect.LED_OFF, 0)
+    gonect.SetLed(gonect.LED_OFF, device_index)
 }
--- gonect.go
+++ gonect.go
@@ -65,6 +65,32 @@ const (
 	LED_BLINK_RED_YELLOW = 6
 )
 
+type VideoFormat uint
+const (
+    FREENECT_VIDEO_RGB             = 0 /**< Decompressed RGB mode (demosaicing
+										 done by libfreenect) */
+    FREENECT_VIDEO_BAYER           = 1 /**< Bayer compressed mode (raw 
+										 information from camera) */
+    FREENECT_VIDEO_IR_8BIT         = 2 /**< 8-bit IR mode  */
+    FREENECT_VIDEO_IR_10BIT        = 3 /**< 10-bit IR mode */
+    FREENECT_VIDEO_IR_10BIT_PACKED = 4 /**< 10-bit packed IR mode */
+    FREENECT_VIDEO_YUV_RGB         = 5 /**< YUV RGB mode */
+    FREENECT_VIDEO_YUV_RAW         = 6 /**< YUV Raw mode */
+//    FREENECT_VIDEO_DUMMY           = 2147483647 /**< Dummy value to force enum
+//												  to be 32 bits wide */
+)
+
+type DepthFormat uint
+const (
+    FREENECT_DEPTH_11BIT        = 0 /**< 11 bit depth information in one uint16_t/pixel */
+    FREENECT_DEPTH_10BIT        = 1 /**< 10 bit depth information in one uint16_t/pixel */
+    FREENECT_DEPTH_11BIT_PACKED = 2 /**< 11 bit packed depth information */
+    FREENECT_DEPTH_10BIT_PACKED = 3 /**< 10 bit packed depth information */
+    FREENECT_DEPTH_REGISTERED   = 4 /**< processed depth data in mm, aligned to 640x480 RGB */
+    FREENECT_DEPTH_MM           = 5 /**< depth to each pixel in mm, but left unaligned to RGB image */
+//    FREENECT_DEPTH_DUMMY        = 2147483647 /**< Dummy value to force enum to be 32 bits wide */
+)
+
 func ConvertCTiltStructToGo(c_ts *C.freenect_raw_tilt_state) TiltState {
 	ts := TiltState{
 		int16(c_ts.accelerometer_x),
@@ -89,20 +115,20 @@ func Init() uint {
 	return uint(C.freenect_init_proxy())
 }
 
-func GetVideo(device_index int) (unsafe.Pointer, uint32) {
+func GetVideo(device_index int, format VideoFormat) (unsafe.Pointer, uint32) {
 	var data unsafe.Pointer
 	var timestamp C.uint32_t
-	out := C.freenect_sync_get_video(&data, &timestamp, C.int(device_index), C.FREENECT_VIDEO_RGB)
+	out := C.freenect_sync_get_video(&data, &timestamp, C.int(device_index), C.freenect_video_format(format))
 	if out > 0 {
 		return nil, 0
 	}
     return data, uint32(timestamp)
 }
 
-func GetDepth(device_index int) (unsafe.Pointer, uint32) {
+func GetDepth(device_index int, format VideoFormat) (unsafe.Pointer, uint32) {
 	var data unsafe.Pointer
 	var timestamp C.uint32_t
-	out := C.freenect_sync_get_depth(&data, &timestamp, C.int(device_index), C.FREENECT_DEPTH_11BIT)
+	out := C.freenect_sync_get_depth(&data, &timestamp, C.int(device_index), C.freenect_depth_format(format))
 	if out > 0 {
 		return nil, 0
 	}
