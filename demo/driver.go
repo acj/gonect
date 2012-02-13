7efda5ecee46dc826e5c2b9d830a9a9ee15133c8
Adam Jensen <acj@linuxguy.org>
2012-02-12 21:37:37 -0500

    Finish basic implementation of RGB, depth, and IR camera code.
    * Corrected incorrect bit depth, number of channels, etc.
    * Removed dependency on golang's opencv wrapper (uses native calls instead)
    * Added RGB-to-BGR conversion to work properly with opencv

--- demo/driver.go
+++ demo/driver.go
@@ -31,18 +31,17 @@ package driver
 import "C"
 
 import (
-    "freenect"
-    "fmt"
-    "time"
-	"opencv"
+	"freenect"
+	"fmt"
+	"time"
 	"unsafe"
 )
 
 func Run() {
 	freenect.Init()
 	fmt.Println("Number of devices: ", freenect.GetNumDevices())
-    //TestTilting(0)
-    //TestLed(0)
+	TestTilting(0)
+	TestLed(0)
 	TestVideo(0)
 	TestIR(0)
 	TestDepth(0)
@@ -52,65 +51,62 @@ func Run() {
 
 func TestVideo(device_index int) {
 	fmt.Println("Testing RGB video. Press ESC to stop.")
-	win := opencv.NewWindow("image")
 	for {
-		data, _ := freenect.GetVideo(0, freenect.FREENECT_VIDEO_RGB)
-		cedge := opencv.CreateImage(640, 480, opencv.IPL_DEPTH_8U, 3)
+		data, _ := freenect.GetVideo(device_index, freenect.FREENECT_VIDEO_RGB)
+		cedge := C.cvCreateImageHeader(C.cvSize(640,480), 8, 3)
 		C.cvSetData(unsafe.Pointer(cedge), data, C.int(1*3*640))
-		win.ShowImage(cedge)
-		if opencv.WaitKey(10) == 27 {
+		C.cvCvtColor(unsafe.Pointer(cedge), unsafe.Pointer(cedge), C.CV_RGB2BGR)
+		C.cvShowImage(C.CString("RGB"), unsafe.Pointer(cedge))
+		if C.cvWaitKey(10) == 27 {
 			return
-        }
+		}
 	}
 }
 
 func TestIR(device_index int) {
-	fmt.Println("Testing depth. Press ESC to stop.")
-	win := opencv.NewWindow("image")
+	fmt.Println("Testing infrared. Press ESC to stop.")
 	for {
-		data, _ := freenect.GetDepth(0, freenect.FREENECT_VIDEO_IR_8BIT)
-		cedge := opencv.CreateImage(640, 488, opencv.IPL_DEPTH_8U, 1)
+		data, _ := freenect.GetVideo(device_index, freenect.FREENECT_VIDEO_IR_8BIT)
+		cedge := C.cvCreateImageHeader(C.cvSize(640,480), 8, 1)
 		C.cvSetData(unsafe.Pointer(cedge), data, C.int(1*1*640))
-		win.ShowImage(cedge)
-		if opencv.WaitKey(10) == 27 {
+		C.cvShowImage(C.CString("IR"), unsafe.Pointer(cedge))
+		if C.cvWaitKey(10) == 27 {
 			return
-        }
+		}
 	}
 }
 
 func TestDepth(device_index int) {
 	fmt.Println("Testing depth. Press ESC to stop.")
-	win := opencv.NewWindow("image")
 	for {
 		data, _ := freenect.GetDepth(0, freenect.FREENECT_DEPTH_11BIT)
-		cedge := opencv.CreateImage(640, 480, opencv.IPL_DEPTH_8U, 1)
-		defer cedge.Release()
-		C.cvSetData(unsafe.Pointer(cedge), data, C.int(1*1*640))
-		win.ShowImage(cedge)
-		if opencv.WaitKey(10) == 27 {
+		cedge := C.cvCreateImageHeader(C.cvSize(640,480), 16, 1)
+		C.cvSetData(unsafe.Pointer(cedge), data, C.int(1*2*640))
+		C.cvShowImage(C.CString("Depth"), unsafe.Pointer(cedge))
+		if C.cvWaitKey(10) == 27 {
 			return
-        }
+		}
 	}
 }
 
 func TestTilting(device_index int) {
-    fmt.Println("Tilting down")
-    freenect.SetTiltDegs(-30, 0)
+	fmt.Println("Tilting down")
+	freenect.SetTiltDegs(-30, 0)
 	ts := freenect.TiltStatusCode(0)
 	for i := 0; i < 3; i++ {
 		time.Sleep(1000000000)
 		ts = freenect.GetTiltStatus(freenect.GetTiltState(0), device_index)
 		fmt.Println("\tTilt status: ", ts, "(", freenect.GetTiltDegs(freenect.GetTiltState(0)), " degrees)")
 	}
-    fmt.Println("Tilting up")
-    freenect.SetTiltDegs(30, 0)
+	fmt.Println("Tilting up")
+	freenect.SetTiltDegs(30, 0)
 	for i := 0; i < 3; i++ {
 		time.Sleep(1000000000)
 		ts = freenect.GetTiltStatus(freenect.GetTiltState(0), device_index)
 		fmt.Println("\tTilt status: ", ts, "(", freenect.GetTiltDegs(freenect.GetTiltState(0)), " degrees)")
 	}
-    fmt.Println("Tilting level")
-    freenect.SetTiltDegs(0, 0)
+	fmt.Println("Tilting level")
+	freenect.SetTiltDegs(0, 0)
 	for i := 0; i < 3; i++ {
 		time.Sleep(1000000000)
 		ts = freenect.GetTiltStatus(freenect.GetTiltState(0), device_index)
@@ -121,26 +117,26 @@ func TestTilting(device_index int) {
 func TestLed(device_index int) {
 	fmt.Println("Changing LED status")
 	fmt.Println("\tOFF")
-    freenect.SetLed(freenect.LED_OFF, device_index)
-    time.Sleep(1000000000)
+	freenect.SetLed(freenect.LED_OFF, device_index)
+	time.Sleep(1000000000)
 	fmt.Println("\tGREEN")
-    freenect.SetLed(freenect.LED_GREEN, device_index)
-    time.Sleep(1000000000)
+	freenect.SetLed(freenect.LED_GREEN, device_index)
+	time.Sleep(1000000000)
 	fmt.Println("\tRED")
-    freenect.SetLed(freenect.LED_RED, device_index)
-    time.Sleep(1000000000)
+	freenect.SetLed(freenect.LED_RED, device_index)
+	time.Sleep(1000000000)
 	fmt.Println("\tYELLOW")
-    freenect.SetLed(freenect.LED_YELLOW, device_index)
-    time.Sleep(1000000000)
+	freenect.SetLed(freenect.LED_YELLOW, device_index)
+	time.Sleep(1000000000)
 	fmt.Println("\tBLINK YELLOW")
-    freenect.SetLed(freenect.LED_BLINK_YELLOW, device_index)
-    time.Sleep(3000000000)
+	freenect.SetLed(freenect.LED_BLINK_YELLOW, device_index)
+	time.Sleep(3000000000)
 	fmt.Println("\tBLINK_GREEN")
-    freenect.SetLed(freenect.LED_BLINK_GREEN, device_index)
-    time.Sleep(3000000000)
+	freenect.SetLed(freenect.LED_BLINK_GREEN, device_index)
+	time.Sleep(3000000000)
 	fmt.Println("\tBLINK RED/YELLOW")
-    freenect.SetLed(freenect.LED_BLINK_RED_YELLOW, device_index)
-    time.Sleep(3000000000)
+	freenect.SetLed(freenect.LED_BLINK_RED_YELLOW, device_index)
+	time.Sleep(3000000000)
 	fmt.Println("\tOFF")
-    freenect.SetLed(freenect.LED_OFF, device_index)
+	freenect.SetLed(freenect.LED_OFF, device_index)
 }
